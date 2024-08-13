0be47f82700ce76c8126d9dd79df42e894b6fd47
adriangabriel1000 <114869991+adriangabriel1000@users.noreply.github.com>
2024-08-13 18:08:27 +0000

    login Done

--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    initals = models.CharField(max_length=10)
+    unique = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    idnum = models.CharField(max_length=200, blank=True)
+    cell = models.CharField(max_length=200, blank=True)
+    homenum = models.CharField(max_length=200, blank=True)
+    dob = models.DateTimeField(blank=True)
+    designation = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(default='profilepic.jpg', upload_to='profile_pictures', blank=True)
+
+    def __str__(self):
+        return self.user.username
