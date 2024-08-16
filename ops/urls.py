732c87c68fa68bf20a7b51fb8795c4a7f726e447
adriangabriel1000 <adriangabriel1000@gmail.com>
2024-08-16 07:39:54 +0200

    m

--- ops/urls.py
+++ ops/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/login.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
-    path('', include(router.urls)),
+    path('', include(router.urls), name='profileapi'),
 ]
 
 urlpatterns += [
