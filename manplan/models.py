5cf1c57e4f749c25437abe1d0d62fcfd1c7c6a7f
adriangabriel1000 <adriangabriel1000@gmail.com>
2024-09-07 20:11:03 +0200

    Starting Manplan

--- manplan/models.py
+++ manplan/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    unNum = models.CharField(max_length=200, blank=True)
+    designation = models.CharField(max_length=200, blank=False)
+    ftm = models.CharField(max_length=3, blank=True)
+    shift = models.CharField(max_length=1, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    idnum = models.CharField(max_length=200, blank=True)
+    cell = models.CharField(max_length=200, blank=True)
+    homenum = models.CharField(max_length=200, blank=True)
+    dob = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Schedule(models.Model):
+    date = models.DateTimeField()
+    position = models.CharField(max_length=5, blank=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date}"
--- manplan/views.py
+++ manplan/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'manplan/manplan.html')
+    return render(request, 'manplan/manplan.html', {
+        'counter': range(100),
+    })
