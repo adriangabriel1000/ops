3c5223ad167081300a5c48b2f783337a8d55ffa2
adriangabriel1000 <adriangabriel1000@gmail.com>
2024-09-10 20:28:44 +0200

    m

--- manplan/views.py
+++ manplan/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from .models import *
 import string
 import random
+from datetime import datetime, timedelta
 
 # Create your views here.
 def index(request):
     employees = Employee.objects.all()
+    schedules = Schedule.objects.all()
+    aSh = Schedule.objects.filter(employee__shift__contains='A', date=datetime.today())
+
     aEmployees = Employee.objects.filter(shift='A')
     bEmployees = Employee.objects.filter(shift='B')
     cEmployees = Employee.objects.filter(shift='C')
@@ -13,7 +17,31 @@ def index(request):
     eEmployees = Employee.objects.filter(shift='E')
     fEmployees = Employee.objects.filter(shift='F')
     oEmployees = Employee.objects.filter(shift='O')
-    print(aEmployees)
+    aList = []
+    # for aEm in aEmployees:
+    #     print(aEm)
+    
+    
+    
+    # print(aSh)
+    # print(datetime.now().date())
+    for sh in aSh:
+        print(sh)
+
+    dateList = []
+    # print(datetime(datetime.today().year,datetime.today().month,datetime.today().day -5,12,1,1 ))
+    # for x in range(-7,42):
+    #     dateList.append(datetime.today() + timedelta(x))
+        # todayList.append(datetime(datetime.today().year,datetime.today().month,datetime.today().day + x,12,1,1 ))
+    # print(dateList)
+
+
+    # for schedule in schedules:
+    #     if datetime.now().date() == schedule.date.date():
+    #         print(schedule.position)
+    
+
+
     return render(request, 'manplan/manplan.html', {
         'counter': range(100),
         'employees': employees,
