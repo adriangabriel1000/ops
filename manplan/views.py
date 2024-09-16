b5eee6a22006a98662f06939e0106d0fd55e471c
adriangabriel1000 <adriangabriel1000@gmail.com>
2024-09-16 11:49:18 +0200

    m

--- manplan/views.py
+++ manplan/views.py
@@ -1,89 +1,77 @@
 from django.shortcuts import render, redirect
 from .models import *
+from cycle.models import Cycle
 import string
 import random
-from datetime import datetime, timedelta, timezone
-from collections import Counter
-import operator 
+import math
+from datetime import datetime, timedelta
 
 
 # Create your views here.
 def index(request):
-    employees = Employee.objects.all()
-    schedules = Schedule.objects.all()
-    aSh = Schedule.objects.filter(employee__shift__contains='A') #filter(employee__shift__contains='A', date__contains=datetime.today().date())
-    aEmployees = Employee.objects.filter(shift='A')
-    bEmployees = Employee.objects.filter(shift='B')
-    cEmployees = Employee.objects.filter(shift='C')
-    dEmployees = Employee.objects.filter(shift='D')
-    eEmployees = Employee.objects.filter(shift='E')
-    fEmployees = Employee.objects.filter(shift='F')
-    oEmployees = Employee.objects.filter(shift='O')
-    aList = {}
+    # employees = Employee.objects.all()
+    # aEmployees = Employee.objects.filter(shift='A')
+    # bEmployees = Employee.objects.filter(shift='B')
+    # cEmployees = Employee.objects.filter(shift='C')
+    # dEmployees = Employee.objects.filter(shift='D')
+    # eEmployees = Employee.objects.filter(shift='E')
+    # fEmployees = Employee.objects.filter(shift='F')
+    # oEmployees = Employee.objects.filter(shift='O')
 
-    # for sh in aSh:
-    #     print(sh.employee)
-    aTemp = []
     dateList = []
     for x in range(-7,42):
         dateList.append(datetime.today().date() + timedelta(x))
 
-# ------------ A Shift ---------------------------------------
-    
-    aEmpCount = {}
-    for emp in aEmployees:
-        aEmpCount.update({emp.name: Schedule.objects.filter(employee__name__contains=emp).count()})
+    # ----------------------------Calculating Shift Row-----------------------------
+    # print(dateList)
+    cycle = Cycle.objects.all()
+    cycleList = []
+    ref = 0
+    # for date in dateList:
+    #     print(date)
+    # print(Cycle.objects.filter(id=ref))
+    # for cyc in cycle:   
+    #     # print(cyc.date.date())
+    #     print(str(cyc.id) + ' ' + str(cyc.date.date()) + ' ' + str(cyc.bShift))
 
+    for date in dateList:
+        finalMNTH = date - datetime(2024,1,1,12,0,0).date() 
+        ref = finalMNTH.days - (math.trunc(finalMNTH.days / 42) * 42) + 3
+        cycleList.append({date: (Cycle.objects.filter(id=ref).values('bShift')[0]['bShift'])})
 
-    
+        # print(str(date) + ' - ' + str(ref) + ' - ' + str(Cycle.objects.filter(id=ref).values('bShift')[0]['bShift']))
 
-    # print(aEmpCount)
-    # print(aEmpCount.get('Hairy Potter'))
-    # print(Schedule.objects.filter(employee__name__contains='Hairy Potter').count())
-
-    # print(aSh.values_list('employee'))                
-    empShList = []
-    # for a in aSh:
-    #     empShList.append(str(a.employee))
-
-    # print(operator.countOf(empShList, 'Hairy Potter'))
-    # print(sum('Hairy Potter' in str(a.employee) for a in aSh))
-    counter = 0
-    for emp in aEmployees:
-        counter = aEmpCount.get(emp.name)
-        for a in aSh:
-            
-            if a.employee == emp:
-                for date in dateList:
-                    if a.date.date() == date:
-                        if a.employee == emp:
-                            aTemp.append(a.position)
-                    else:
-                        aTemp.append(' ')
-
-            
-
-        aList.update({emp: aTemp}) 
-        aTemp=[]
-                    
-
-    # print(aList)
-    
-#---------------------- End A Shift ----------------------------------
         
+
+    print(cycleList)
+    # ----------------------------Calculating Shift Row-----------------------------
+
+    # aList = plan('A', dateList)
+    # bList = plan('B', dateList)
+    # cList = plan('C', dateList)
+    # dList = plan('D', dateList)
+    # eList = plan('E', dateList)
+    # fList = plan('F', dateList)
+    # oList = plan('O', dateList)
 
     return render(request, 'manplan/manplan.html', {
         'counter': range(49),
-        'employees': employees,
-        'aEmployees': aEmployees,
-        'bEmployees': bEmployees,
-        'cEmployees': cEmployees,
-        'dEmployees': dEmployees,
-        'eEmployees': eEmployees,
-        'fEmployees': fEmployees,
-        'oEmployees': oEmployees,
+        # 'employees': employees,
+        # 'aEmployees': aEmployees,
+        # 'bEmployees': bEmployees,
+        # 'cEmployees': cEmployees,
+        # 'dEmployees': dEmployees,
+        # 'eEmployees': eEmployees,
+        # 'fEmployees': fEmployees,
+        # 'oEmployees': oEmployees,
         'dateList': dateList,
-        'aList': aList,
+        'aList': plan('A', dateList),
+        'bList': plan('B', dateList),
+        'cList': plan('C', dateList),
+        'dList': plan('D', dateList),
+        'eList': plan('E', dateList),
+        'fList': plan('F', dateList),
+        'oList': plan('O', dateList),
     })
 
 
@@ -103,3 +91,28 @@ def pop():
         print(fullname)
 
     return employee
+
+
+def plan(shft, dlist):
+    aTemp = []
+    aEmpCount = {}
+    aList = {}
+    for emp in Employee.objects.filter(shift=shft):
+        aEmpCount.update({emp.name: Schedule.objects.filter(employee__name__contains=emp).count()})
+
+    for emp in Employee.objects.filter(shift=shft):
+        tempEmp = {}
+        empTemp = {}
+        tempEmp = Schedule.objects.filter(employee=emp)
+        for tem in tempEmp:
+            empTemp.update({tem.date.date(): tem.position})
+
+        for date in dlist:
+            if date in empTemp:
+                aTemp.append(empTemp[date])
+            else:
+                aTemp.append(' ')
+    
+        aList.update({emp: aTemp}) 
+        aTemp=[]    
+    return aList
